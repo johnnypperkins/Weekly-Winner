0ac6cc246880af3e2161f4e48aa5b5399c198c73
johnnypperkins <johnnynzperkins@gmail.com>
2023-05-26 11:21:13 -0700

    Initial Commit

---
+++ Weekly Winner/Weekly_WinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weekly_WinnerApp.swift
+//  Weekly Winner
+//
+//  Created by Johnny Perkins on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Weekly_WinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
