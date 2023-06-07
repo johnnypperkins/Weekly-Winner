3dff85829d82ed0a55fe07bb80da793916b62c73
Reid Brown <56902716+reidbrown1@users.noreply.github.com>
2023-06-06 18:40:53 -0700

    test - Reid

--- Weekly Winner/Weekly_WinnerApp.swift
+++ Weekly Winner/Weekly_WinnerApp.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import FirebaseCore
 
+// Reid Was Here
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
--- Weekly Winner/screen2.swift
+++ Weekly Winner/screen2.swift
@@ -15,6 +15,7 @@ enum GameType: String, CaseIterable {
 struct BettingAppView: View {
     @State private var selectedGameType = GameType.collegeFootball
     @ObservedObject private var viewModel = bookViewModel()
+    @State private var showingSheet = false
     
     var body: some View {
         NavigationView {
@@ -39,12 +40,9 @@ struct BettingAppView: View {
                 
                 ScrollView {
                     VStack(spacing: 20) {
-                        ForEach(viewModel.NFLgames) { game in
-                            NavigationLink(destination: BetDetailsView(bet: createSampleBet())) {
-                                //BetRowView1(game: game)
-                                Text(game.awayTeam)
+                        ForEach(viewModel.NFLgames, id: \.idd) { game in
+                            BetRowView1(game: game)
                             }
-                        }
                     }
                     .padding()
                 }
@@ -58,132 +56,85 @@ struct BettingAppView: View {
                 .edgesIgnoringSafeArea(.all)
             )
             .navigationBarHidden(true)
-        }.onAppear(perform: {viewModel.getGames()})
-    }
-    
-    private var filteredGames: [Game] {
-        switch selectedGameType {
-        case .collegeFootball:
-            return collegeFootballGames
-        case .nfl:
-            return nflGames
         }
     }
+    
+    /*private var filteredGames: [Game] {
+        switch selectedGameType {
+        case .collegeFootball:
+            return ""
+        case .nfl:
+            return ""
+        }
+    }*/
 }
 
 struct BetRowView1: View {
-    let game: game
-    
-    var body: some View {
-        Text("g")
-    }
-}
-
-struct Game: Identifiable, Hashable {
-    let id = UUID()
-    let teams: [Team]
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: Game, rhs: Game) -> Bool {
-        lhs.id == rhs.id
-    }
-}
-
-struct Team: Identifiable {
-    let id = UUID()
-    let name: String
-    let spread: String
-    let overUnder: String
-}
-
-struct Bet1: Identifiable {
-    let id = UUID()
-    let teams: [Team]
-}
-
-// Sample data for college football games
-let collegeFootballGames: [Game] = [
-    Game(teams: [
-        Team(name: "Team A", spread: "+5", overUnder: "20"),
-        Team(name: "Team B", spread: "-5", overUnder: "18")
-    ]),
-    Game(teams: [
-        Team(name: "Team C", spread: "+2", overUnder: "15"),
-        Team(name: "Team D", spread: "-2", overUnder: "25")
-    ])
-    // Additional college football games...
-]
-
-// Sample data for NFL games
-let nflGames: [Game] = [
-    Game(teams: [
-        Team(name: "Team X", spread: "+7", overUnder: "30"),
-        Team(name: "Team Y", spread: "-7", overUnder: "28")
-    ]),
-    Game(teams: [
-        Team(name: "Team W", spread: "+3", overUnder: "22"),
-        Team(name: "Team Z", spread: "-3", overUnder: "24")
-    ])
-    // Additional NFL games...
-]
-
-// Helper function to create a sample bet
-func createSampleBet() -> Bet1 {
-    return Bet1(teams: [
-        Team(name: "Team A", spread: "+5", overUnder: "20"),
-        Team(name: "Team B", spread: "-5", overUnder: "18")
-    ])
-}
-
-struct BetDetailsView: View {
-    let bet: Bet1
-    
-    var body: some View {
-        VStack {
-            Text("Bet Details")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding()
-            
-            ForEach(bet.teams) { team in
-                Text("Team: \(team.name)")
-                    .font(.headline)
-                
-                Text("Spread: \(team.spread)")
-                    .font(.subheadline)
-                
-                Text("Over/Under: \(team.overUnder)")
-                    .font(.subheadline)
-                
-                Divider()
-            }
-            
-            Spacer()
-        }
-    }
-}
-
-struct BetItemView: View {
-    let team: Team
+    let game: Game
+    @State private var showingAway = false
+    @State private var showingTotal = false
+    @State private var showingHome = false
+    @State private var showingSheet = false
     
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                Text(team.name)
-                    .font(.headline)
-                    .foregroundColor(.white)
+                HStack{
+                    Text(game.awayTeam)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    Spacer()
+                    
+                    Text("@\(game.homeTeam)")
+                        .foregroundColor(.white)
+                }
                 
                 HStack(spacing: 10) {
-                    Text("Spread: \(team.spread)")
-                        .font(.subheadline)
-                        .foregroundColor(.white)
-                    
-                    Text("Over/Under: \(team.overUnder)")
-                        .font(.subheadline)
-                        .foregroundColor(.white)
+                    Button(action: {
+                        self.showingAway.toggle()
+                        self.showingSheet.toggle()
+                            }) {
+                                Text("\(game.awaySpread, specifier: "%.1f")")
+                                    .foregroundColor(.blue)
+                                    .padding()
+                                    .background(Color(showingAway ? .gray : .white))
+                                    .cornerRadius(10)
+                                    .shadow(color: showingAway ? .gray : .clear, radius: 5)
+                                    .scaleEffect(showingAway ? 0.9 : 1.0)
+                                    .animation(.spring(), value: 4)
+                            }
+                    Spacer()
+                    Button(action: {
+                                self.showingTotal.toggle()
+                        self.showingSheet.toggle()
+                    }) {
+                        HStack{
+                            Image(systemName: "arrow.up.arrow.down")
+                                .padding(.leading)
+                            Text("\(game.totalOU, specifier: "%.1f")")
+                                .foregroundColor(.blue)
+                                .padding()
+                        }
+                        .background(Color(showingTotal ? .gray : .white))
+                        .cornerRadius(10)
+                        .shadow(color: showingTotal ? .gray : .clear, radius: 5)
+                        .scaleEffect(showingTotal ? 0.9 : 1.0)
+                        .animation(.spring(), value: 4)
+                    }
+                    Spacer()
+                    Button(action: {
+                                self.showingHome.toggle()
+                        self.showingSheet.toggle()
+                            }) {
+                                Text("\(game.homeSpread, specifier: "%.1f")")
+                                    .foregroundColor(.blue)
+                                    .padding()
+                                    .background(Color(showingHome ? .gray : .white))
+                                    .cornerRadius(10)
+                                    .shadow(color: showingHome ? .gray : .clear, radius: 5)
+                                    .scaleEffect(showingHome ? 0.9 : 1.0)
+                                    .animation(.spring(), value: 4)
+                            }
                 }
             }
             .padding(.leading)
@@ -202,8 +153,173 @@ struct BetItemView: View {
                 )
         )
         .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 2)
+        .sheet(isPresented: $showingSheet) {
+            BetDetailsView(game: game, showingAway: $showingAway, showingHome: $showingHome, showingTotal: $showingTotal)
+                .presentationDetents([.medium, .large])
+                .presentationDragIndicator(.hidden)
+                .interactiveDismissDisabled()
+        }
     }
 }
+
+
+
+struct BetDetailsView: View {
+    let game: Game
+    @Binding var showingAway: Bool
+    @Binding var showingHome: Bool
+    @Binding var showingTotal: Bool
+    @Environment(\.dismiss) var dismiss
+    @State private var userRating: Double = 2
+    
+    
+    var body: some View {
+        VStack {
+            HStack{
+                Button {
+                    withAnimation {
+                        dismiss()
+                        showingAway = false
+                        showingHome = false
+                        showingTotal = false
+                    }
+                } label: {
+                    Image(systemName: "arrow.turn.left.up")
+                        .resizable()
+                        .frame(width: 20,height: 20)
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                
+                Text("Create Your Bet")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Spacer()
+                
+            }.frame(maxWidth:.infinity, alignment: .center)
+                .padding(.leading)
+            
+            if showingAway {
+                VStack{
+                    Text("Team: \(game.awayTeam)")
+                        .font(.headline)
+                    
+                    HStack {
+                        Text("Rate This Bar:")
+                            .font(.headline)
+                        Slider(value: $userRating, in: Double(game.awaySpread - 5)...Double(game.awaySpread + 5), step: 0.5) { editing in
+                            if editing == false {
+                                
+                            }
+                        }
+                        .accentColor(Color(.green))
+                        
+                        Text(String(format: "%.1f", userRating))
+                            .font(.headline)
+                    }
+                    .padding()
+                    
+                    Text("Spread: \(game.totalOU)")
+                        .font(.subheadline)
+                    
+                    Text("Over/Under: \(game.awayTeam)")
+                        .font(.subheadline)
+                    
+                    Divider()
+                }}
+            if showingHome {
+                VStack{
+                    Text("Team: \(game.homeTeam)")
+                        .font(.headline)
+                    
+                    HStack {
+                        Text("Rate This Bar:")
+                            .font(.headline)
+                        Slider(value: $userRating, in: Double(game.homeSpread - 5)...Double(game.homeSpread + 5), step: 0.5) { editing in
+                            if editing == false {
+                                
+                            }
+                        }
+                        .accentColor(Color(.green))
+                        
+                        Text(String(format: "%.1f", userRating))
+                            .font(.headline)
+                    }
+                    .padding()
+                    
+                    Text("Spread: \(game.totalOU, specifier: "%.1f")")
+                        .font(.subheadline)
+                    
+                    Text("Over/Under: \(game.awayTeam)")
+                        .font(.subheadline)
+                    
+                    Divider()
+                    
+                    
+                }}
+            
+            if showingTotal {
+                VStack{
+                    Text("Team: \(game.awayTeam)")
+                        .font(.headline)
+                    
+                    HStack {
+                        Text("Rate This Bar:")
+                            .font(.headline)
+                        Slider(value: $userRating, in: Double(game.awaySpread - 5)...Double(game.awaySpread + 5), step: 0.5) { editing in
+                            if editing == false {
+                                
+                            }
+                        }
+                        .accentColor(Color(.green))
+                        
+                        Text(String(format: "%.1f", userRating))
+                            .font(.headline)
+                    }
+                    .padding()
+                    
+                    Text("Spread: \(game.totalOU)")
+                        .font(.subheadline)
+                    
+                    Text("Over/Under: \(game.awayTeam)")
+                        .font(.subheadline)
+                    
+                    Divider()
+                    
+                
+                }}
+            
+            Button(action: {
+                       // Place bet action
+                   }) {
+                       Text("Place Bet")
+                           .font(.title)
+                           .fontWeight(.bold)
+                           .foregroundColor(.white)
+                           .padding()
+                           .frame(maxWidth: .infinity)
+                           .background(
+                               LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading, endPoint: .trailing)
+                           )
+                           .cornerRadius(10)
+                           .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 2)
+                   }
+        }.onAppear(perform: {
+            if showingAway {
+                userRating = game.awaySpread}
+            if showingHome {
+                userRating = game.homeSpread}
+            if showingTotal {
+                userRating = game.totalOU}
+            
+        })
+    }
+    
+}
+
+
 
 struct BettingAppView_Previews: PreviewProvider {
     static var previews: some View {
