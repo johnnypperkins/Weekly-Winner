94ed06c4183f5066879a43dcaff726707304529e
johnnypperkins <johnnynzperkins@gmail.com>
2023-11-12 15:27:52 +0100

    as

--- Weekly Winner/Views/SignupIntro.swift
+++ Weekly Winner/Views/SignupIntro.swift
@@ -18,110 +18,110 @@ struct SignupIntro: View {
     
     
     var body: some View {
-//        ScrollView(.init()){
-        VStack{
-            HStack{
-                Text("WagerPool")
-                    .font(.custom(K.customFonts.lexendDecaSB, size: 35))
-                    .foregroundColor(K.finalColor.titleBlue)
-                Spacer()
-                if pageIndex < 2 {
-                    NavigationLink (destination:{
-                        tabBarView()
-                            .environmentObject(viewModel)
-                    }, label: {
-                        HStack{
-                            
-                            Spacer()
-                            
-                            Text("Skip")
-                                .font(Font.custom(K.customFonts.lexendDecaMedium, size: 12))
+        //        ScrollView(.init()){
+            VStack{
+                HStack{
+                    Text("WagerPool")
+                        .font(.custom(K.customFonts.lexendDecaSB, size: 35))
+                        .foregroundColor(K.finalColor.titleBlue)
+                    Spacer()
+                    if pageIndex < 2 {
+                        NavigationLink (destination:{
+                            tabBarView()
+                                .environmentObject(viewModel)
+                        }, label: {
+                            HStack{
+                                
+                                Spacer()
+                                
+                                Text("Skip")
+                                    .font(Font.custom(K.customFonts.lexendDecaMedium, size: 12))
+                                    .foregroundColor(.white)
+                                //shadow
+                                
+                                Spacer()
+                            }.frame(minWidth: 0, maxWidth: 60, minHeight: 30 , maxHeight: 30)
+                                .background(Color(red: 0.31, green: 0.57, blue: 1))
+                                .cornerRadius(10)
+                                .padding(16)
+                        }).onSubmit {
+                            viewModel.showMainScreen()
+                        }
+                    }
+                }.padding(.leading,15)
+                TabView(selection: $pageIndex){
+                    Intro1(pageIndex: $pageIndex)
+                        .tag(0)
+                    Intro2(pageIndex: $pageIndex)
+                        .tag(1)
+                    Intro3(pageIndex: $pageIndex)
+                        .tag(2)
+                    //                ForEach(colors.indices, id: \.self) { index in
+                    //
+                    //                    Text("Hello world")
+                    //                }
+                }.shadow(color: .black, radius: 10, x: 2, y: 2)
+                    .animation(.easeInOut, value: pageIndex)
+                    .tabViewStyle(.page)
+                    .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+                HStack{
+                    Spacer()
+                    if pageIndex < 2 {
+                        Button {
+                            withAnimation{
+                                pageIndex += 1
+                            }
+                        } label: {
+                            Image(systemName: "arrow.forward")
+                                .resizable()
+                                .frame(width: 35, height: 30)
+                                .foregroundColor(Color(red: 0.31, green: 0.57, blue: 1))
+                        }
+                    } else {
+                        NavigationLink (destination: {
+                            tabBarView()
+                                .environmentObject(viewModel)
+                        }, label: {
+                            Text("Continue")
+                                .font(Font.custom(K.customFonts.lexendDecaMedium, size: 14))
                                 .foregroundColor(.white)
                             //shadow
                             
-                            Spacer()
-                        }.frame(minWidth: 0, maxWidth: 60, minHeight: 30 , maxHeight: 30)
-                            .background(Color(red: 0.31, green: 0.57, blue: 1))
-                            .cornerRadius(10)
-                            .padding(16)
-                    }).onSubmit {
-                        viewModel.showMainScreen()
-                    }
-                }
-            }.padding(.leading,15)
-            TabView(selection: $pageIndex){
-                Intro1(pageIndex: $pageIndex)
-                    .tag(0)
-                Intro2(pageIndex: $pageIndex)
-                    .tag(1)
-                Intro3(pageIndex: $pageIndex)
-                    .tag(2)
-                //                ForEach(colors.indices, id: \.self) { index in
-                //
-                //                    Text("Hello world")
-                //                }
-            }.shadow(color: .black, radius: 10, x: 2, y: 2)
-            .animation(.easeInOut, value: pageIndex)
-                .tabViewStyle(.page)
-                .indexViewStyle(.page(backgroundDisplayMode: .interactive))
-            HStack{
-                Spacer()
-                if pageIndex < 2 {
-                    Button {
-                        withAnimation{
-                            pageIndex += 1
+                            
+                        }).onSubmit {
+                            viewModel.showMainScreen()
                         }
-                    } label: {
-                        Image(systemName: "arrow.forward")
-                            .resizable()
-                            .frame(width: 35, height: 30)
-                            .foregroundColor(Color(red: 0.31, green: 0.57, blue: 1))
-                    }
-                } else {
-                    NavigationLink (destination: {
-                        tabBarView()
-                            .environmentObject(viewModel)
-                    }, label: {
-                        Text("Continue")
-                            .font(Font.custom(K.customFonts.lexendDecaMedium, size: 14))
-                            .foregroundColor(.white)
-                        //shadow
-                        
-                       
-                    }).onSubmit {
-                        viewModel.showMainScreen()
-                    }
-                    .frame(minWidth: 0, maxWidth: 80, minHeight: 40 , maxHeight: 40)
+                        .frame(minWidth: 0, maxWidth: 80, minHeight: 40 , maxHeight: 40)
                         .background(Color(red: 0.31, green: 0.57, blue: 1))
                         .cornerRadius(10)
-                    
-                }
-            }.padding(.horizontal, 25)
-                .padding(.top,20)
-                .padding(.bottom,50)
-
+                        
+                    }
+                }.padding(.horizontal, 25)
+                    .padding(.top,20)
+                    .padding(.bottom,50)
+                
+                
+                //                .overlay(
+                //                    HStack(spacing: 15) {
+                //                        ForEach(colors.indices, id: \.self) { index in
+                //                            Capsule()
+                //                                .fill(Color.white)
+                //                                .frame(width: 7, height: 7)
+                //                        }
+                //                    }
+                //                        .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                //                        .padding(.bottom,10)
+                //                    ,alignment: .bottom
+                //                )
+            }.ignoresSafeArea()
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                .background(Color(red: 0.02, green: 0.05, blue: 0.26))
+                .padding(.top,45)
+                .navigationBarBackButtonHidden(true)
             
-            //                .overlay(
-            //                    HStack(spacing: 15) {
-            //                        ForEach(colors.indices, id: \.self) { index in
-            //                            Capsule()
-            //                                .fill(Color.white)
-            //                                .frame(width: 7, height: 7)
-            //                        }
-            //                    }
-            //                        .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
-            //                        .padding(.bottom,10)
-            //                    ,alignment: .bottom
-            //                )
-        }.ignoresSafeArea()
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-            .background(Color(red: 0.02, green: 0.05, blue: 0.26))
-            .padding(.top,45)
-            .navigationBarBackButtonHidden(true)
-   
+            
         
     }
-    
     func nextPage() {
         pageIndex += 1
     }
