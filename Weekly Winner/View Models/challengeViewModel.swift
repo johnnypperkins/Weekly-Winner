7755de6fac0b37be56484c72df38eaaaf285ce2e
Reid Brown <reid.brown1@gmail.com>
2024-01-01 23:53:27 -0500

    starting challenges

---
+++ Weekly Winner/View Models/challengeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  challengeViewModel.swift
+//  Weekly Winner
+//
+//  Created by Reid Brown (Test) on 1/1/24.
+//
+
+import Foundation
---
+++ Weekly Winner/Views/challengeView.swift
@@ -0,0 +1,8 @@
+//
+//  challengeView.swift
+//  Weekly Winner
+//
+//  Created by Reid Brown (Test) on 1/1/24.
+//
+
+import Foundation
