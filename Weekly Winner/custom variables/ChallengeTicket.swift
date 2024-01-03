d7af271a65cd739a24021229020237eadb258a92
johnnypperkins <johnnynzperkins@gmail.com>
2024-01-03 13:05:12 -0800

    mm

--- Weekly Winner/custom variables/ChallengeTicket.swift
+++ Weekly Winner/custom variables/ChallengeTicket.swift
@@ -9,12 +9,11 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
-struct HeadToHeadBet: Identifiable, Codable {
+struct ChallengeBet: Identifiable, Codable {
     @DocumentID var id: String?
     var username: String
     var uid: String
     var groupID: String
-    var groupNumber: Int
     var dateCreated: Timestamp // will fix later
     var totalWon: Int
     var totalPotentialWon: Int
