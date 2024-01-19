a335133ea2de508e92c746bacfd29fae537ce095
johnnypperkins <johnnynzperkins@gmail.com>
2024-01-19 12:29:06 -0500

    editss

---
+++ Weekly Winner/Views/Challenge Views/challengeDetails.swift
@@ -0,0 +1,18 @@
+//
+//  challengeDetails.swift
+//  Weekly Winner
+//
+//  Created by Johnny Perkins on 1/17/24.
+//
+
+import SwiftUI
+
+struct challengeDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    challengeDetails()
+}
