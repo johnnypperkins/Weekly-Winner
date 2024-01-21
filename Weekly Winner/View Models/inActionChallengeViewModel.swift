16c96fea41187f1ab1dec2578389d11eee7d0f0c
johnnypperkins <johnnynzperkins@gmail.com>
2024-01-21 12:25:12 -0500

    dsds

---
+++ Weekly Winner/View Models/inActionChallengeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  inActionChallengeViewModel.swift
+//  Weekly Winner
+//
+//  Created by Johnny Perkins on 1/21/24.
+//
+
+import Foundation
---
+++ Weekly Winner/Views/Challenge Views/inActionChallengeView.swift
@@ -0,0 +1,18 @@
+//
+//  inActionChallengeView.swift
+//  Weekly Winner
+//
+//  Created by Johnny Perkins on 1/21/24.
+//
+
+import SwiftUI
+
+struct inActionChallengeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    inActionChallengeView()
+}
